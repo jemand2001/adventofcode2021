694e3c348415ba433bf9b437105be268b14fda41
jemand2001 <>
2021-12-12 21:46:58 +0100

    commentary

--- Day12.hs
+++ Day12.hs
@@ -51,10 +51,11 @@ findPaths2 :: ([Edge], [Cave]) -> [[Cave]]
 findPaths2 (es, caves) = findPaths' [] Start
   where
     findPaths' path End
-      | 1 < countBy ((1 <) . count path) (filter isSmall caves) = []
+      | 1 < countBy ((1 <) . count path) (filter isSmall caves) = []  -- if there are multiple double small caves, discard the path
       | otherwise = [path]
     findPaths' current start = concatMap (findPaths' (start : current)) [start' | P caveA start' <- es, caveA == start, satisfiesReq start' current]
     satisfiesReq (Big _) _ = True
+    -- for some reason this doesn't always catch double doubles (if you know why, please leave an issue or something)
     satisfiesReq c@(Small _) path = c `notElem` path || all (\c' -> count path c' < 2) (filter isSmall caves)
     satisfiesReq _ _ = True
 
